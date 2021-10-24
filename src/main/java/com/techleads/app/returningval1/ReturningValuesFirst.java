16428320f8f327e2d4c67de8bc24ec3f78ce71f8
madhav <anupoju.anand@gmail.com>
2021-10-24 22:07:41 +0530

    returning value

--- src/main/java/com/techleads/app/returningval1/ReturningValuesFirst.java
+++ src/main/java/com/techleads/app/returningval1/ReturningValuesFirst.java
@@ -3,7 +3,31 @@ package com.techleads.app.returningval1;
 public class ReturningValuesFirst {
 	
 	public static void main(String[] args) {
-		
+		//pool-N-thread-M
+				String currentThreadName = Thread.currentThread().getName();
+				System.out.println("[ " + currentThreadName + " ] Main thread starts here...");
+
+				ValueReturningTaskA tk1=new ValueReturningTaskA(2,3, 2000);
+				Thread t1=new Thread(tk1, "Thread-1");
+				
+				
+				ValueReturningTaskA tk2=new ValueReturningTaskA(3,4, 1000);
+				Thread t2=new Thread(tk2, "Thread-2");
+				
+				ValueReturningTaskA tk3=new ValueReturningTaskA(4,5, 500);
+				Thread t3=new Thread(tk3, "Thread-3");
+				
+				t1.start();
+				t2.start();
+				t3.start();
+				
+
+				System.out.println("Result-1 = "+tk1.getSum());
+				System.out.println("Result-1 = "+tk2.getSum());
+				System.out.println("Result-1 = "+tk3.getSum());
+				
+				System.out.println("[ " + currentThreadName + " ] Main thread ends here...");
+
 	}
 
 }
--- src/main/java/com/techleads/app/returningval1/ValueReturningTaskA.java
+++ src/main/java/com/techleads/app/returningval1/ValueReturningTaskA.java
@@ -1,17 +1,62 @@
 package com.techleads.app.returningval1;
 
-public class ValueReturningTaskA implements Runnable {
-	
-	
+import java.util.concurrent.TimeUnit;
 
-	public ValueReturningTaskA() {
+public class ValueReturningTaskA implements Runnable {
+
+	private int a;
+	private int b;
+	private long sleepTime;
+	private int sum;
+	
+	private static int count=0;
+	private int instanceNumber;
+	private String taskId;
+
+	private volatile boolean done=false;
+
+	public ValueReturningTaskA(int a, int b, long sleepTime) {
 		super();
-		// TODO Auto-generated constructor stub
+		this.a = a;
+		this.b = b;
+		this.sleepTime = sleepTime;
+		this.instanceNumber= ++count;
+		this.taskId="ValueReturningTaskA-"+instanceNumber;
 	}
+
 
 	@Override
 	public void run() {
 
+		System.out.println("#### [" + Thread.currentThread().getName() + "] <TASK-" + taskId + "> STARTING #####");
+		try {
+			TimeUnit.MILLISECONDS.sleep(sleepTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sum=a+b;
+		System.out.println("#### [" + Thread.currentThread().getName() + "] <TASK-" + taskId + "> COMPLETED #####");
+		done=true;
+		synchronized (this) {
+			System.out.println("{" + Thread.currentThread().getName() + "} Notifying with Id: " + taskId + "...");
+			this.notify();
+		}
+	}
+	
+	public int getSum() {
+		synchronized (this) {
+			if(!done) {
+				try {
+					System.out.println("{" + Thread.currentThread().getName() + "} Waiting for the result from " + taskId + "...");
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		return sum;
 	}
 
 }
